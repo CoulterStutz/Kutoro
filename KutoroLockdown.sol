c6684f41d10ea82f5ea2d141140c82f7343fc50d
coulterminer <coultercash@gmail.com>
2022-08-13 01:10:27 -0600

    Update KutoroLockdown.sol
    used as a way to shield important on chain contracts deployed by Kutoro which are necessary to the network from attack

--- KutoroLockdown.sol
+++ KutoroLockdown.sol
@@ -1,1 +1,21 @@
+pragma solidity ^0.8.0;
 
+contract KutoroLockdownSystem{
+    bool isCurrentlyLocked;
+
+    constructor(){
+        isCurrentlyLocked = false;
+    }
+
+
+    function ToggleLockdown(bool option) public returns (bool success) {
+        require(msg.sender == 0xa693190103733280E23055BE70C838d9b6708b9a);
+
+        isCurrentlyLocked = option;
+        return true;
+    }
+
+    function isLocked() public view returns (bool isLocked) {
+        return isCurrentlyLocked;
+    }
+}
